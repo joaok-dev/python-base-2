a9905fd056ca51206ca4ac719ae9748b878c14af
jkocx <joaoksoliveira1@gmail.com>
2023-09-18 20:18:34 -0300

    created the first part of the multi-language hello.py script

--- hello.py
+++ hello.py
@@ -1,3 +1,38 @@
 #!/usr/bin/env python3
+"""Hello World Multi Languages.
 
-print("Hello World")
+Depending on the language configured in the envoronment, the script displays the 
+corresponding message.
+
+
+Usage:
+Ensure the LANG variable is properly configured.
+
+    export LANG=pt_BR
+
+Execution:
+    python3 hello.py
+    or
+    ./hello.py
+"""
+
+__version__ = "0.1.0"
+__author__ = "joaok"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+elif current_language == "es_ES":
+    msg = "Hola, Mundo!"
+elif current_language == "fr_FR":
+    msg = "Bonjour, Monde!"
+
+print(msg)
