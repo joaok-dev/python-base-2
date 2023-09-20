adc4b050b352dae519b2d1662676526993e9fa42
jkocx <joaoksoliveira1@gmail.com>
2023-09-20 18:15:38 -0300

    Added CLI args and STDIN for language selection in hello.py

--- hello.py
+++ hello.py
@@ -4,10 +4,15 @@
 Depending on the language configured in the envoronment, the script displays the 
 corresponding message.
 
+
 Usage:
 Ensure the LANG variable is properly configured.
 
     export LANG=pt_BR
+
+Or provide via the CLI with the --lang argument.
+Or the user will have to type.
+
 
 Execution:
     python3 hello.py
@@ -19,10 +24,50 @@ __version__ = "0.1.2"
 __author__ = "joaok"
 __license__ = "Unlicense"
 
+# Import the os and sys modules for environment variable and
+# command line argument handling.
 import os
+import sys
 
-current_language = os.getenv("LANG", "en_US")[:5]
+# Initialize a dictionary to hold the command-line arguments.
+arguments = {
+    "lang": None,
+    "count": 1,
+}
 
+# Loop through the command line arguments starting from index 1
+# (ignoring the script name).
+for arg in sys.argv[1:]:
+    # TODO: Handle ValueError
+    key, value = arg.split("=")
+    # Remove leading hyphens and whitespace from the key.
+    key = key.lstrip("-").strip()
+    # Strip whitespace from the value.
+    value = value.strip()
+
+    # Validate the argument key.
+    if key not in arguments:
+        print("Invalid Argument {key}")
+        # Exit the program if the key is not recognized.
+        sys.exit()
+
+    # Store the value into the arguments dictionary.
+    arguments[key] = value
+
+# Get the language preference from the arguments or fall back
+# to the environment variable.
+current_language = arguments["lang"]
+if current_language is None:
+    # TODO: Implement a loop for persistent language setting.
+    if "LANG" in os.environ:
+        current_language = os.getenv("LANG")
+    else:
+        current_language = input("Choose a language:")
+
+# Trim the language code to first 5 characters (language and region).
+current_language = current_language[:5]
+
+# Message dictionary containing greeting messages in multiple languages.
 msg = {
     "en_US": "Hello, World!",
     "pt_BR": "Olá, mundo!",
@@ -31,4 +76,5 @@ msg = {
     "fr_FR": "Bonjour, Monde!",
 }
 
-print(msg[current_language])
+# Print the greeting message.
+print(msg[current_language] * int(arguments["count"]))
