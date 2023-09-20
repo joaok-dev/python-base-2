aa756ed5ffbaec33db0b75e242238b8608b597aa
jkocx <joaoksoliveira1@gmail.com>
2023-09-20 15:22:19 -0300

    Replaced a chain of if-statements with dicts loopkup in hello.py

--- hello.py
+++ hello.py
@@ -15,7 +15,7 @@ Execution:
     ./hello.py
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.2"
 __author__ = "joaok"
 __license__ = "Unlicense"
 
@@ -23,15 +23,12 @@ import os
 
 current_language = os.getenv("LANG", "en_US")[:5]
 
-msg = "Hello, World!"
+msg = {
+    "en_US": "Hello, World!",
+    "pt_BR": "Olá, mundo!",
+    "it_IT": "Ciao, Mondo!",
+    "es_ES": "Hola, Mundo!",
+    "fr_FR": "Bonjour, Monde!",
+}
 
-if current_language == "pt_BR":
-    msg = "Olá, mundo!"
-elif current_language == "it_IT":
-    msg = "Ciao, Mondo!"
-elif current_language == "es_ES":
-    msg = "Hola, Mundo!"
-elif current_language == "fr_FR":
-    msg = "Bonjour, Monde!"
-
-print(msg)
+print(msg[current_language])
