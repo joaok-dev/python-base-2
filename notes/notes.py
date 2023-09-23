3ab5d1ed79cd1bf78f0ffa1e19a42fd5901977b9
joao k. oliveira <joaoksoliveira1@gmail.com>
2023-09-22 22:02:49 -0300

    separated each script in respective folder

---
+++ notes/notes.py
@@ -0,0 +1,34 @@
+"""Notepad
+
+$ notes.py new "My note"
+tag: tech
+text: 
+General anotation about tech carrer
+
+$ notes.py read --tag=tech
+...
+...
+
+"""
+__version__ = "0.1.0"
+__author__ = "joaok"
+
+import sys
+
+arguments = sys.argv[1:]
+
+if not arguments:
+    print("Invalid usage")
+    sys.exit(1)
+
+validated_arguments = ["new", "read"]
+
+if arguments not in validated_arguments:
+    print("Invalid Argument")
+    sys.exit(1)
+
+if arguments[0] == "read":
+    ...
+
+if arguments[0] == "new":
+    ...
